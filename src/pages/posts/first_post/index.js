601d177f6a982975d04e02b001fad8ab0e82f13d
Debora Nisenbaum <debora.nisenbaum@nubank.com.br>
2023-04-18 17:03:04 -0300

    adds blog structure

---
+++ src/pages/posts/first_post/index.js
@@ -0,0 +1,7 @@
+import Image from 'next/image';
+
+export default function FirstPost() {
+  return (
+    <Image src="/images/profile.jpg" height={144} width={144} alt="raio x muito loco" />
+  )
+}
